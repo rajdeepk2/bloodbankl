65a51fa2c64fb6ec79f2ad3e70868b2de02468ab
rajdeepk2 <rajdeepk2357@gmail.com>
2024-04-23 00:05:36 +0530

    first commit

---
+++ routes/testRoutes.js
@@ -0,0 +1,6 @@
+const express=require('express');
+const { testController } = require('../controllers/testController');
+const router=express.Router();
+router.get('/',testController);
+
+module.exports=router;
